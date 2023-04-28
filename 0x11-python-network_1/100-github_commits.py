e76dbf29acb54ed80bd107981822162ae08b8d3f
GiddyLesGid <gideonlangat@gmail.com>
2023-04-28 04:33:53 -0700

    network 2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 """
-Python script that shows the last 10 commits of a repository
-in GitHub
+Python script that lists the 10 most recent commits of a GitHub repository
 """
 import requests
 import sys
 
-
-def get_commits(owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    response = requests.get(url)
-    if response.ok:
-        return response.json()
-    else:
-        response.raise_for_status()
-
-
 if __name__ == "__main__":
-    try:
-        owner = sys.argv[1]
-        repo = sys.argv[2]
-        commits = get_commits(owner, repo)[:10]
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()[:10]
         for commit in commits:
-            print(f"{commit['sha']}: {commit['commit']['author']['name']}")
-    except (IndexError, requests.exceptions.HTTPError):
-        pass
-
-
+            sha = commit.get("sha")
+            author_name = commit.get("commit").get("author").get("name")
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"Error: {response.status_code}")
