d22eeb4b5685141b64f0ce9eab99df818f019122
GiddyLesGid <gideonlangat@gmail.com>
2023-04-28 03:35:06 -0700

    network 2

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-
-"""
-Displays the X-Request-Id header variable of a request to a given URL.
-The X-Request-Id header is a unique identifier that is often used in web applications
-to track requests and responses between the client and server. It allows developers
-to trace the path of a request through various systems and diagnose issues that may
-arise during processing. 
-
-Usage: python3 script.py <url>
-
-Example: python3 script.py https://www.example.com
+"""Displays the X-Request-Id header variable of a request to a given URL
 """
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
