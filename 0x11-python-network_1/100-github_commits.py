856369a18cc81f92d6c0bbfde3320b7ac0d139a7
GiddyLesGid <gideonlangat@gmail.com>
2023-04-28 04:14:47 -0700

    network 2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,47 +1,22 @@
 #!/usr/bin/python3
 """
-Lists the 10 most recent commits on a given GitHub repository.
+Python script that shows the last 10 commits of a repository
+in GitHub
 """
-
+from requests import get, auth
 import sys
-import requests
-
-
-def get_recent_commits(owner, repo):
-    """
-    Retrieves the 10 most recent commits of a GitHub repository.
-
-    Args:
-        owner (str): The repository owner's GitHub username.
-        repo (str): The name of the repository.
-
-    Returns:
-        List of dictionaries representing the 10 most recent commits. Each
-        dictionary contains the commit's SHA hash and author name.
-    """
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    r = requests.get(url)
-    if r.status_code != 200:
-        print(f"Error: Could not retrieve commits from {url}")
-        sys.exit(1)
-    commits = r.json()
-    recent_commits = []
-    for i in range(min(10, len(commits))):
-        commit_info = {"sha": commits[i].get("sha"),
-                       "author": commits[i].get("commit").get("author").get("name")}
-        recent_commits.append(commit_info)
-    return recent_commits
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./recent_commits.py <owner> <repo>")
-        sys.exit(1)
-
-    owner = sys.argv[1]
-    repo = sys.argv[2]
-    recent_commits = get_recent_commits(owner, repo)
-
-    for commit in recent_commits:
-        print("{}: {}".format(commit["sha"], commit["author"]))
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
 
