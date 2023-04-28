70ec0799ec0182b449228ec5f03eaa1e185249fc
GiddyLesGid <gideonlangat@gmail.com>
2023-04-28 03:31:23 -0700

    network 2

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+
 """
-A script that fetches https://alx-intranet.hbtn.io/status using the requests package
+Displays the X-Request-Id header variable of a request to a given URL.
+The X-Request-Id header is a unique identifier that is often used in web applications
+to track requests and responses between the client and server. It allows developers
+to trace the path of a request through various systems and diagnose issues that may
+arise during processing. 
+
+Usage: python3 script.py <url>
+
+Example: python3 script.py https://www.example.com
 """
 
+import sys
 import requests
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
