19e2104fcb24d9d7f51260cb6c899a7e3561d191
GiddyLesGid <gideonlangat@gmail.com>
2023-05-23 05:40:53 -0700

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
-const API_URL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(API_URL, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+request(url + process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-
-  if (response.statusCode !== 200) {
-    console.log('Error:', response.statusCode);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  console.log(film.title);
 });
 
